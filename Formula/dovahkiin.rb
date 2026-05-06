4196202c82ce0ee0fd2267a3f5e838037ef72aef
Marco Sandoval <mrsandvv@gmail.com>
2026-05-06 17:21:58 -0600

    dovahkiin 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/dovahkiin.rb
+++ Formula/dovahkiin.rb
@@ -1,8 +1,8 @@
 class Dovahkiin < Formula
   desc "A cowsay-like CLI that shouts Skyrim quotes from Alduin"
   homepage "https://spacehole.tech"
-  url "https://github.com/mrsandv/dovahkiin-say/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/mrsandv/dovahkiin-say/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "de9531caa732997cf0aecdb23d3823d431c428813e3bdf080e1626160d2c2a3b"
   license "MIT"
 
   depends_on "rust" => :build
