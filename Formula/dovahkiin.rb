a63e08447a3c6d5228ed6400ae509761b14b7de8
Marco Sandoval <marco@commandostudio.com>
2026-05-07 12:31:03 -0600

    feat: switch to pre-compiled binaries

--- Formula/dovahkiin.rb
+++ Formula/dovahkiin.rb
@@ -1,14 +1,12 @@
 class Dovahkiin < Formula
   desc "A cowsay-like CLI that shouts Skyrim quotes from Alduin"
   homepage "https://spacehole.tech"
-  url "https://github.com/mrsandv/dovahkiin-say/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "de9531caa732997cf0aecdb23d3823d431c428813e3bdf080e1626160d2c2a3b"
+  url "https://github.com/mrsandv/dovahkiin-say/releases/download/v0.1.2/dovahkiin-macos.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "dovahkiin"
     man1.install "dovahkiin.1"
   end
 
