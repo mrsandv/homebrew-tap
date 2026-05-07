6016a5b3cf190d2642091f2648c3f80af4964580
Marco Sandoval <mrsandvv@gmail.com>
2026-05-07 12:49:48 -0600

    chore: update dovahkiin to v0.1.3

--- Formula/dovahkiin.rb
+++ Formula/dovahkiin.rb
@@ -1,8 +1,8 @@
 class Dovahkiin < Formula
   desc "A cowsay-like CLI that shouts Skyrim quotes from Alduin"
   homepage "https://spacehole.tech"
-  url "https://github.com/mrsandv/dovahkiin-say/releases/download/v0.1.2/dovahkiin-macos.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/mrsandv/dovahkiin-say/releases/download/v0.1.3/dovahkiin-macos.tar.gz"
+  sha256 "7d76042d347b2dbc4f7a08253b1663bd44be1c7dcbe18108aa5b72de9946cf34"
   license "MIT"
 
   def install
